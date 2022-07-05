391bafeee67dcf3a1ff9e30d18bd106b130c6cf9
Yogesh <yogeshbietjhs@gmail.com>
2022-07-05 16:31:20 +0530

    inicial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace stacksAndQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Stacks and Queues");
+            Stack<int> stack = new Stack<int>();
+            stack.Push(70);
+            stack.Push(30);  
+            stack.Push(56);
+            stack.PeeK();
+            stack.Pop();
+            stack.PeeK();
+
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,26 @@
+﻿namespace stacksAndQueues
+{
+    public class Stack<T>
+    {
+        LinkedList<T> linkedList;
+        public Stack()
+        {
+            this.linkedList = new LinkedList<T>();
+        }
+        public void Push(T data)
+        {
+            linkedList.AddFirst(data);
+        }
+        //LIFO last element will pop out
+        public void Pop()
+        {
+            linkedList.RemoveFirst();
+        }
+
+        //peek method will show thefirst elemrnt in the Linklist, does not delete the node from the ll
+        public void PeeK()
+        {
+            Console.WriteLine(linkedList.First());
+        }
+    }
+}
